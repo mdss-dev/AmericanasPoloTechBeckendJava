9c88f97db87eff3a779e058e2e83200bad7cd74e
Michael Douglas <douglas.ti.dev@gmail.com>
2022-12-11 19:58:03 -0300

    Package Aula_02 e classe menorValor criada

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import Aula_01.*;
+import Aula_02.MenorValor;
 
 public class Main {
     public static void main(String[] args) {
-        ConversorDeTemperatura.conversor();
-        TiposDeDados.tiposDeDados();
+//        ConversorDeTemperatura.conversor();
+//        TiposDeDados.tiposDeDados();
+        MenorValor.recebeValores();
     }
 }
