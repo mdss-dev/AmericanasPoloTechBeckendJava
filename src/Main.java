6c4bdd2c3e4312893fdc40da6698fd7baf0ae7d4
Michael Douglas <douglas.ti.dev@gmail.com>
2022-12-12 18:16:26 -0300

    Package Aula_03 e classe Operaçoes criada

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import Aula_01.*;
 import Aula_02.MenorValor;
+import Aula_03.Operacoes;
 
 public class Main {
     public static void main(String[] args) {
 //        ConversorDeTemperatura.conversor();
 //        TiposDeDados.tiposDeDados();
-        MenorValor.recebeValores();
+//        MenorValor.recebeValores();
+        Operacoes.operacoes();
     }
 }
