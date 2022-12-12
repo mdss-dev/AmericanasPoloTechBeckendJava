f856e84a12abfdd5a1942abb58036fd0abeac7f0
Michael Douglas <douglas.ti.dev@gmail.com>
2022-12-12 18:33:17 -0300

    Classe MediaOperações criada no Package Aula_03

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import Aula_01.*;
-import Aula_02.MenorValor;
-import Aula_03.Operacoes;
+import Aula_02.*;
+import Aula_03.*;
 
 public class Main {
     public static void main(String[] args) {
 //        ConversorDeTemperatura.conversor();
 //        TiposDeDados.tiposDeDados();
 //        MenorValor.recebeValores();
-        Operacoes.operacoes();
+//        Operacoes.operacoes();
+        MediaArray.main();
     }
 }
